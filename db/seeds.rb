f2f33d127ca226d6d37c2ca0d30e178a4c638e66
Norfa Bagas <bgsnorfa@gmail.com>
2020-08-20 06:58:28 +0700

    Add seed for user and add space in html title

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def seeds_user
+  User.create({
+    name: 'admin',
+    email: 'admin@mail.com',
+    password: 'admin123',
+    password_confirmation: 'admin123'
+  })
+end
+
+seeds_user
