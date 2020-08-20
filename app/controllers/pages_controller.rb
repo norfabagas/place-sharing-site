1992732bc6eaad3ccf135f990d26a0f603cec235
Norfa Bagas <bgsnorfa@gmail.com>
2020-08-20 19:28:01 +0700

    Add profile page feature

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,11 @@ class PagesController < ApplicationController
   def profile
     @user = User.where('username = ?', params[:username]).first
     if @user.nil?
-      return redirect_to :back
+      return redirect_to root_path
     end
+  end
+
+  def user
+    @user = User.find(current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   devise_for :users, :controllers => { :registrations => "registrations" }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/profile', to: 'pages#user', as: 'user_page'
+
+  # place this url below for non-ambiguous routing
   get '/:username', to: 'pages#profile', as: 'profile_page'
 end
